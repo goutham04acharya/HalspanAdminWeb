b889310b3bc001207216a24ce851817f8b61ff95
Nayana <nayana.sk@7edge.com>
2024-07-16 17:11:03 +0530

    RR-298 #time 10m feature:resolved merge conflicts

--- src/Pages/auth/AuthPOC/Login.jsx
+++ src/Pages/auth/AuthPOC/Login.jsx
@@ -1,17 +1,4 @@
-// import React from 'react';
-// import { useAuth0 } from "@auth0/auth0-react";
 
-// function Login() {
-//   const { loginWithRedirect } = useAuth0();
-
-//   return (
-//     <div>
-//       <button onClick={() => loginWithRedirect()}>Log in</button>
-//     </div>
-//   );
-// }
-
-// export default Login;
 import React, { useEffect } from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
 
