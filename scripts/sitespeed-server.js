3393a0356fa7648212c842735937648c3b6dbc68
Nishanth S Raj <nishanth.raj@7edge.com>
2024-07-17 12:29:53 +0530

    RR-349 #time 1m ci: gitignore updated

--- scripts/sitespeed-server.js
+++ scripts/sitespeed-server.js
@@ -1,11 +1,19 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Necessary for __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
-app.listen(5174);
+app.listen(5174, () => {
+    console.log('Server is listening on port 5174');
+});
