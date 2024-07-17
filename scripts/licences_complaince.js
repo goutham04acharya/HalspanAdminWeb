13dccd6af6598e17da7113640b755824fdb0ec51
Nishanth S Raj <nishanth.raj@7edge.com>
2024-07-17 10:41:02 +0530

    RR-349 #time 1m ci: license file is updated

--- scripts/licences_complaince.js
+++ scripts/licences_complaince.js
@@ -1,16 +1,16 @@
-const fs = require('fs');
+import { readFileSync, writeFileSync } from 'fs';
 
 // Read the package.json file
-const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+const packageJson = JSON.parse(readFileSync('package.json', 'utf8'));
 
 // Extract the license and description information
 const packageInfo = {};
 for (const dependency in packageJson.dependencies) {
-    const depPackageJson = JSON.parse(fs.readFileSync(`node_modules/${dependency}/package.json`, 'utf8'));
+    const depPackageJson = JSON.parse(readFileSync(`node_modules/${dependency}/package.json`, 'utf8'));
     packageInfo[`${dependency}@${packageJson.dependencies[dependency]}`] = {
         license: depPackageJson.license,
         description: depPackageJson.description
     };
 }
 
-fs.writeFileSync('licenses.json', JSON.stringify(packageInfo, null, 2));
+writeFileSync('licenses.json', JSON.stringify(packageInfo, null, 2));
