3b8815aeb9ec70a7e47c53fc12ac9e66233bf8fc
Rahul N Vaster <rahul.vaster@7edge.com>
2024-07-23 13:25:04 +0530

    RR-349 #time 1m ci: unit testing update

--- test/step-definations/Driver.js
+++ test/step-definations/Driver.js
@@ -20,7 +20,7 @@ options.addArguments('--disable-gpu');
 options.addArguments('--disable-extensions');
 options.addArguments('--dns-prefetch-disable');
 options.addArguments('enable-features=NetworkServiceInProcess');
-setDefaultTimeout(24000);
+setDefaultTimeout(34000);
 
 global.driver = chrome.Driver.createSession(options, service);
 
@@ -93,3 +93,26 @@ AfterStep(async function () {
         console.log(`error:${err}`);
     }
 })
+After(function (scenario) {
+    console.log('scenario.result.status',scenario.result.status)
+    let failed_scenarios = path.join(__dirname, 'failed_scenarios');
+    if (!fs.existsSync(failed_scenarios)) {
+        fs.mkdirSync(failed_scenarios);
+    }
+    if (scenario.result.status === 'FAILED') {
+        var world = this;
+        return driver.takeScreenshot().then(function(screenShot, error) {
+            if (!error) {
+                world.attach(screenShot, "image/png");
+                failed_scenarios = path.join(failed_scenarios,`${scenario.pickle.id}_${scenario.pickle.name.replaceAll('/', '_')}.png`)
+                fs.writeFile(failed_scenarios, screenShot, 'base64', (err) => {
+                    if (err) {
+                        console.error('Error writing coverage data:', err);
+                    } else {
+                        console.log('Coverage data has been written to:', failed_scenarios);
+                    }
+                });
+            }
+        });
+    }
+});
