0945596fda4a724ef92ebc1dd828551e6b37f5e4
Roopesh <roopesh.yadava@7edge.com>
2024-07-30 09:58:52 +0530

    RR-300 #time 1h 30m feature: Added the testcases for the organize multiple sections and pages

--- test/integration-testing/Flow1/cucumber.js
+++ test/integration-testing/Flow1/cucumber.js
@@ -3,7 +3,9 @@ module.exports = {
         import: ['test'],
         paths: [
             'test/integration-testing/Flow1/1_login.feature',
+            'test/integration-testing/Flow1/4_view_list_questionnaires.feature',
             'test/integration-testing/Flow1/3_create_questionnaries.feature',
+            'test/integration-testing/Flow1/5_organize_sections_pages.feature',
             'test/integration-testing/Flow1/2_logout.feature'
         ]
     }
