06646d8abf456af38260e12791b0a7bf68d768a1
Nayana <nayana.sk@7edge.com>
2024-07-31 15:39:16 +0530

    RR-303 #time 30m feature:debugged and resolved bdd issue

--- src/Pages/QuestionnariesList/Components/Table.jsx
+++ src/Pages/QuestionnariesList/Components/Table.jsx
@@ -40,8 +40,8 @@ function Table({ loading, QueList, lastElementRef }) {
                     <th className='min-w-[200px] text-start px-10 py-6 font-medium text-base text-[#2B333B]'>ID</th>
                     <th className='min-w-[300px] text-start py-6 font-medium text-base text-[#2B333B]'>INTERNAL NAME</th>
                     <th className='min-w-[400px] text-start py-6 font-medium text-base text-[#2B333B]'>PUBLIC NAME</th>
-                    <th className='min-w-[200px] text-start py-6 font-medium text-base text-[#2B333B]'>STATUS</th>
-                    <th className='min-w-[200px] text-start py-6 font-medium text-base text-[#2B333B]'>ASSET TYPE type</th>
+                    <th className='min-w-[200px] text-start py-6 font-medium text-base text-[#2B333B] pl-[18px]'>STATUS</th>
+                    <th className='min-w-[200px] text-start py-6 font-medium text-base text-[#2B333B]'>ASSET TYPE</th>
                     <th className='min-w-[500px] text-start py-6 font-medium text-base text-[#2B333B]'>DESCRIPTION</th>
                 </thead>
                 {loading
