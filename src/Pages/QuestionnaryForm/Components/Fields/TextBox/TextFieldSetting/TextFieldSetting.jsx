db77a27c4b60dcd5a2a952dcecacb8c9401b9404
Nayana <nayana.sk@7edge.com>
2024-08-08 14:35:08 +0530

    RR-439 #time 20m feature:POC is in progress

--- src/Pages/QuestionnaryForm/Components/Fields/TextBox/TextFieldSetting/TextFieldSetting.jsx
+++ src/Pages/QuestionnaryForm/Components/Fields/TextBox/TextFieldSetting/TextFieldSetting.jsx
@@ -4,7 +4,7 @@ function TestFieldSetting() {
   return (
     <div className='py-[34px] px-[32px]'>
       <p className='font-semibold text-[#2B333B] text-[22px]'>Field settings</p>
-      <div className='mt-[14px]'>
+      <div className='mt-[14px] h-customh9 overflow-auto default-sidebar'>
         <div className='flex flex-col justify-start'>
           <label htmlFor="Label" className='font-semibold text-base text-[#2B333B]'>Label</label>
           <input type="text" id='Label' className='mt-[11px] border border-[#AEB3B7] rounded py-[11px] px-4 font-normal text-base text-[#2B333B] placeholder:text-[#9FACB9] outline-0'
@@ -40,13 +40,13 @@ function TestFieldSetting() {
               <input type='radio'
                 className='w-[17px] h-[17px]'
                 name='type'
-                id='Dropdown'
-                value='Dropdown'
+                id='Singleline'
+                value='Singleline'
               // checked={createDetails.type === 'Yes'}
               // onChange={handleadhocChange} 
               />
               <label htmlFor='yes' className='ml-7 font-normal text-base text-[#2B333B] cursor-pointer'>
-                Dropdown
+                Single line
               </label>
             </div>
             <div className="relative custom-radioBlue flex items-center mt-3" data-testid='no'
@@ -60,7 +60,7 @@ function TestFieldSetting() {
               // onChange={handleadhocChange} 
               />
               <label htmlFor='no' className='ml-7 font-normal text-base text-[#2B333B] cursor-pointer'>
-                Single Choice
+                Multi-line
               </label>
             </div>
             <div className="relative custom-radioBlue flex items-center mt-3" data-testid='no'
@@ -73,7 +73,7 @@ function TestFieldSetting() {
               // onChange={handleadhocChange} 
               />
               <label htmlFor='no' className='ml-7 font-normal text-base text-[#2B333B] cursor-pointer'>
-                Multi Choice
+                Lookup
               </label>
             </div>
           </div>
@@ -81,14 +81,14 @@ function TestFieldSetting() {
         <div className='mt-7'>
           <p className='font-semibold text-[#2B333B] text-base'>Source</p>
           <div className='flex flex-col justify-start mt-7 w-full relative'>
-          <label htmlFor="Label" className='font-semibold text-base text-[#2B333B]'>Default Content</label>
-          <div className='relative w-full'>
-            <input type="text" id='Label' className='mt-[11px] w-full border border-[#AEB3B7] rounded py-[11px] pl-4 pr-11 font-normal text-base text-[#2B333B] placeholder:text-[#9FACB9] outline-0'
-              placeholder='Populates the content'
-            />
-            <img src="/Images/setting.svg" alt="setting" className='absolute top-5 right-3 cursor-pointer' />
+            <label htmlFor="Label" className='font-semibold text-base text-[#2B333B]'>Default Content</label>
+            <div className='relative w-full'>
+              <input type="text" id='Label' className='mt-[11px] w-full border border-[#AEB3B7] rounded py-[11px] pl-4 pr-11 font-normal text-base text-[#2B333B] placeholder:text-[#9FACB9] outline-0'
+                placeholder='Populates the content'
+              />
+              <img src="/Images/setting.svg" alt="setting" className='absolute top-5 right-3 cursor-pointer' />
+            </div>
           </div>
-        </div>
         </div>
       </div>
     </div>
