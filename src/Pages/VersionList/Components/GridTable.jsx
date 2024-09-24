4decd38eac355d83dd0ea01c1e8c4070b6ed0c68
Raheel Nazeer Ahmed <raheel.ahmed@7edge.com>
2024-09-24 18:35:09 +0530

    RR-537 #time 1h feature: Fixed some UI issues

--- src/Pages/VersionList/Components/GridTable.jsx
+++ src/Pages/VersionList/Components/GridTable.jsx
@@ -6,7 +6,7 @@ export function GridTable({ setVersionList, versionList, loading, lastElemendive
     const { PatchAPI } = useApi();
     const options = ['Draft', 'Testing', 'Published', 'Retired'];
     const [dropdownsOpen, setDropdownsOpen] = useState({});
-    const dropdownRef = useRef();
+    const dropdownRefs = useRef([]);
 
     const handleDropdownClick = (version_number) => {
         setDropdownsOpen((prev) => ({
@@ -59,32 +59,20 @@ export function GridTable({ setVersionList, versionList, loading, lastElemendive
         } catch (error) {
             console.error('Error updating status:', error);
         }
+        
         // Close dropdown after selecting an option
         setDropdownsOpen((prev) => ({
             ...prev,
             [version_number]: false,
         }));
     };
-    const getStatusStyles = (status) => {
-        switch (status) {
-            case 'Draft':
-                return 'bg-[#D6DDEC] text-[#2B333B]';
-            case 'Testing':
-                return 'bg-[#F8F0DE] text-[#2B333B]';
-            case 'Published':
-                return 'bg-[#DEF4E1] text-[#2B333B]';
-            case 'Retired':
-                return 'bg-[#E8D7D7] text-[#2B333B]';
-            default:
-                return '-'; // Default styles when status doesn't match any condition
-        }
-    };
 
     useEffect(() => {
         const handleClickOutside = (event) => {
-            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-                setDropdownsOpen({});
+            if (dropdownRefs.current.some(ref => ref && ref.contains(event.target))) {
+                return;
             }
+            setDropdownsOpen({});
         };
 
         document.addEventListener('mousedown', handleClickOutside);
@@ -92,23 +80,24 @@ export function GridTable({ setVersionList, versionList, loading, lastElemendive
             document.removeEventListener('mousedown', handleClickOutside);
         };
     }, []);
+
     return (
         <div className='overflow-auto default-sidebar h-customh11'>
-            <div className='flex flex-col w-full'>
+            <div className='flex flex-col'>
                 <div className='sticky top-0 bg-white z-[99]'>
-                    <div className='flex w-full z-[99]'>
+                    <div className='flex'>
                         <div className='min-w-[300px] text-start px-10 py-6 font-medium text-base text-[#2B333B]'>NAME</div>
                         <div className='min-w-[300px] text-start px-10 py-6 font-medium text-base text-[#2B333B]'>LAST EDITED</div>
                         <div className='min-w-[300px] text-start px-10 py-6 font-medium text-base text-[#2B333B]'>EDITED BY</div>
-                        <div className='min-w-[300px] bg-white text-start px-10 py-6 font-medium text-base text-[#2B333B]'>STATUS</div>
+                        <div className='min-w-[300px] text-start px-10 py-6 font-medium text-base text-[#2B333B]'>STATUS</div>
                     </div>
                 </div>
                 {loading
-                    ? <Shimmer column={4} row={10} firstIndex version />
+                    ? <Shimmer column={4} row={10} firstIndex />
                     : <div className='bg-white'>
                         {versionList?.data?.items.map((versionListInfo, index) => (
                             <React.Fragment key={index}>
-                                <div className='rounded-[10px] mt-[18px] flex w-full relative' ref={dropdownRef}>
+                                <div className='rounded-[10px] mt-[18px] flex w-full relative' ref={el => dropdownRefs.current[index] = el}>
                                     <div className='py-6 text-start truncate min-w-[300px] bg-[#F4F6FA] px-10 rounded-tl-[10px] rounded-bl-[10px] font-semibold text-base text-[#2B333B] cursor-pointer'>
                                         <u>Version {versionListInfo?.version_number}</u>
                                     </div>
@@ -122,12 +111,12 @@ export function GridTable({ setVersionList, versionList, loading, lastElemendive
                                     <div className='py-6 text-start truncate min-w-[300px] bg-[#F4F6FA] px-10 font-normal text-base text-[#2B333B]'>
                                         {versionListInfo?.updated_by?.name}
                                     </div>
-                                    <div className='py-6 text-start truncate min-w-[300px] bg-[#F4F6FA] px-10 rounded-tr-[10px] rounded-br-[10px] font-normal text-base text-[#2B333B]' onClick={() => handleDropdownClick(versionListInfo?.version_number)}>
+                                    <div className='py-6 text-start truncate min-w-[300px] bg-[#F4F6FA] px-10 rounded-tr-[10px] rounded-br-[10px] font-normal text-base text-[#2B333B]'>
                                         {versionListInfo?.status ? (
                                             <div>
-                                                {(versionListInfo?.status === 'Draft' || versionListInfo?.status === 'Testing') ? (
+                                                {(versionListInfo?.status === 'Draft' || versionListInfo?.status === 'Retired') && (
                                                     <div className='flex w-[164px] h-[36px] border border-[#AEB3B7] rounded px-[18px] bg-white items-center cursor-pointer'
-                                                        >
+                                                    onClick={() => handleDropdownClick(versionListInfo?.version_number)}>
                                                         <input
                                                             type="text"
                                                             id={versionListInfo?.version_number}
@@ -141,10 +130,6 @@ export function GridTable({ setVersionList, versionList, loading, lastElemendive
                                                             className={`w-5 transition-transform duration-300 text-[#2B333B] ${dropdownsOpen[versionListInfo?.version_number] ? 'rotate-180' : 'rotate-0'}`}
                                                         />
                                                     </div>
-                                                ) : (
-                                                    <span className={`py-2 px-4 rounded-[15px] ${getStatusStyles(versionListInfo?.status)}`}>
-                                                        {versionListInfo?.status}
-                                                    </span>
                                                 )}
                                                 {dropdownsOpen[versionListInfo?.version_number] && (
                                                     <ul className="absolute bg-white border border-[#AEB3B7] mt-1 w-[164px] top-[58px] z-10">
@@ -163,7 +148,7 @@ export function GridTable({ setVersionList, versionList, loading, lastElemendive
                                         )}
                                     </div>
                                 </div>
-                                {/* <div className='h-4 bg-white'></div> */}
+                                <div className='h-4 bg-white'></div>
                                 <div ref={lastElemendivef} className='h-1' />
                             </React.Fragment>
                         ))}
