3dd31d904806805e0c710f7a2ccd69db827eba6a
Nayana <nayana.sk@7edge.com>
2024-10-09 16:13:18 +0530

    RR-419 #time 12m feature:Admin show hide for entire section pages for number fields advance editor is completed

--- src/Pages/QuestionnaryForm/Components/ConditionalLogicAdvanced/Components/StaticDetails/StaticDetails.jsx
+++ src/Pages/QuestionnaryForm/Components/ConditionalLogicAdvanced/Components/StaticDetails/StaticDetails.jsx
@@ -73,19 +73,19 @@ function StaticDetails({
                         </div>
                         <div className='mt-3'>
                             <p className='font-semibold text-base text-[#000000]'>Smaller</p>
-                            <p className='font-normal text-base text-[#000000]'>=(AssetName &gt 20)</p>
+                            <p className='font-normal text-base text-[#000000]'>=(AssetName &gt; 20)</p>
                         </div>
                         <div className='mt-3'>
                             <p className='font-semibold text-base text-[#000000]'>Larger</p>
-                            <p className='font-normal text-base text-[#000000]'>=(AssetName &lt 20)</p>
+                            <p className='font-normal text-base text-[#000000]'>=(AssetName &lt; 20)</p>
                         </div>
                         <div className='mt-3'>
                             <p className='font-semibold text-base text-[#000000]'>Smaller or Equal</p>
-                            <p className='font-normal text-base text-[#000000]'>=(AssetNumber &lt= 20)</p>
-                        </div>
+                            <p className='font-normal text-base text-[#000000]'>= (AssetNumber &lt;= 20)</p>
+                            </div>
                         <div className='mt-3'>
                             <p className='font-semibold text-base text-[#000000]'>Larger or Equal</p>
-                            <p className='font-normal text-base text-[#000000]'>=(AssetNumber &gt= 20)</p>
+                            <p className='font-normal text-base text-[#000000]'>=(AssetNumber &gt;= 20)</p>
                         </div>
                     </div>
                 )}
@@ -94,19 +94,19 @@ function StaticDetails({
                         <p className='font-semibold text-lg text-[#2B333B]'>Common Yes/No Calculations</p>
                         <div className='mt-3'>
                             <p className='font-semibold text-base text-[#000000]'>Date is before Today</p>
-                            <p className='font-normal text-base text-[#000000]'>=(StartDate &lt "Today")</p>
+                            <p className='font-normal text-base text-[#000000]'>=(StartDate &lt; "Today")</p>
                         </div>
                         <div className='mt-3'>
                             <p className='font-semibold text-base text-[#000000]'>Date is before or equal to Today</p>
-                            <p className='font-normal text-base text-[#000000]'>=StartDate &lt= Today</p>
+                            <p className='font-normal text-base text-[#000000]'>=StartDate &lt;= Today</p>
                         </div>
                         <div className='mt-3'>
                             <p className='font-semibold text-base text-[#000000]'>Date is after Today</p>
-                            <p className='font-normal text-base text-[#000000]'>=(StartDate &gt "Today")</p>
+                            <p className='font-normal text-base text-[#000000]'>=(StartDate &gt; "Today")</p>
                         </div>
                         <div className='mt-3'>
                             <p className='font-semibold text-base text-[#000000]'>Date is after or equal to Today</p>
-                            <p className='font-normal text-base text-[#000000]'>=(StartDate &gt= "Today")</p>
+                            <p className='font-normal text-base text-[#000000]'>=(StartDate &gt;= "Today")</p>
                         </div>
                         <div className='mt-3'>
                             <p className='font-semibold text-base text-[#000000]'>Date is with "X" days of Set date</p>
@@ -120,11 +120,15 @@ function StaticDetails({
                         <p className='font-semibold text-lg text-[#2B333B]'>Common Yes/No Calculations</p>
                         <div className='mt-3'>
                             <p className='font-semibold text-base text-[#000000]'>Has Atleast One File</p>
-                            <p className='font-normal text-base text-[#000000]'>=(UploadDamagedDoor() &lt 0)</p>
+                            <p className='font-normal text-base text-[#000000]'>=(UploadDamagedDoor() &lt; 0)</p>
                         </div>
                         <div className='mt-3'>
                             <p className='font-semibold text-base text-[#000000]'>Has No Files</p>
                             <p className='font-normal text-base text-[#000000]'>=(UploadDamagedDoor() = 0)</p>
+                        </div>
+                        <div className='mt-3'>
+                            <p className='font-semibold text-base text-[#000000]'>Number of Files is</p>
+                            <p className='font-normal text-base text-[#000000]'>=(UploadDamagedDoor() = 5)</p>
                         </div>
                     </div>
                 )}
