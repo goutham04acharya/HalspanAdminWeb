6ae32b55d942851a35fbb87c6032c0e51b3ef842
Roopesh Yadava <roopesh.yadava@7edge.com>
2024-10-10 10:33:00 +0530

    RR-421 #time 20m feature: testcases for files added

--- test/integration-testing/Flow1/cucumber.js
+++ test/integration-testing/Flow1/cucumber.js
@@ -30,6 +30,7 @@ module.exports = {
             'test/integration-testing/Flow1/26_change_status.feature',
             'test/integration-testing/Flow1/27_duplicate_questionnaire.feature',
             'test/integration-testing/Flow1/30_advanced_editor.feature',
+            'test/integration-testing/Flow1/33_files_photos_video_advanced_editor.feature',
             'test/integration-testing/Flow1/2_logout.feature'
         ]
     }
---
+++ test/step-definations/admin/files_photos_video_advanced_editor.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+const { Given, When, Then, But } = require('@cucumber/cucumber')
+const webdriver = require('selenium-webdriver');
+const until = require('selenium-webdriver').until
+const By = require('selenium-webdriver').By
+const Key = webdriver.Key
+
+When('I enter the correct conditional logic for files or photos or videos', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending'; 
+});
+
+Then('I should see the advanced editor for files or photos or videos', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('I enter the incorrect conditional logic for files or photos or videos', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
