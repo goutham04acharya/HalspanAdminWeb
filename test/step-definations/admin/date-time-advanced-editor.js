5683602ba276092089395c98f86c758f364485f8
Ranjith N <ranjith.n@7edge.com>
2024-10-14 08:14:05 +0530

    RR-419 unit test

--- test/step-definations/admin/date-time-advanced-editor.js
+++ test/step-definations/admin/date-time-advanced-editor.js
@@ -26,58 +26,58 @@ When('I select the question from the suggestions for date\\/time field', async f
     // eslint-disable-next-line max-len
     await driver.wait(until.elementLocated(By.css(`[data-testid="conditional-logic-text"]`))).sendKeys('Section_1.Page_1.Date_or_time.setDate(20) AND ', Key.RETURN);
 
-    while (bool) {
-        let suggestionElement = await driver.wait(until.elementLocated(By.css(`[data-testid="suggestion-${i}"]`)), 10000);
-        let suggestionText = await suggestionElement.getText();
-        console.log("suggestionText = ", suggestionText, "suggestions[0] = ", suggestions[0]);
-        if (suggestionText === suggestions[0]) {
-            await suggestionElement.click();
-            bool = false;
-        }
-        i++;
-    }
+    // while (bool) {
+    //     let suggestionElement = await driver.wait(until.elementLocated(By.css(`[data-testid="suggestion-${i}"]`)), 10000);
+    //     let suggestionText = await suggestionElement.getText();
+    //     console.log("suggestionText = ", suggestionText, "suggestions[0] = ", suggestions[0]);
+    //     if (suggestionText === suggestions[0]) {
+    //         await suggestionElement.click();
+    //         bool = false;
+    //     }
+    //     i++;
+    // }
 
-    await driver.wait(until.elementLocated(By.css(`[data-testid="conditional-logic-text"]`))).sendKeys('.');
+    // await driver.wait(until.elementLocated(By.css(`[data-testid="conditional-logic-text"]`))).sendKeys('.');
 
-    let conditions = ['AddDays()',
-        'SubtractDays()',
-        'getFullYear()',
-        'getMonth()',
-        'getDate()',
-        'getDay()',
-        'getHours()',
-        'getMinutes()',
-        'getSeconds()',
-        'getMilliseconds()',
-        'getTime()',
-        'Date()'];
+    // let conditions = ['AddDays()',
+    //     'SubtractDays()',
+    //     'getFullYear()',
+    //     'getMonth()',
+    //     'getDate()',
+    //     'getDay()',
+    //     'getHours()',
+    //     'getMinutes()',
+    //     'getSeconds()',
+    //     'getMilliseconds()',
+    //     'getTime()',
+    //     'Date()'];
 
-    bool = true;
-    i = 0;
+    // bool = true;
+    // i = 0;
 
-    while (i < conditions.length) {
-        let conditionElement = await driver.wait(until.elementLocated(By.css(`[data-testid="condition-${i}"]`)), 10000);
-        let conditionText = await conditionElement.getText();
-        assert.equal(conditionText, conditions[i]);
+    // while (i < conditions.length) {
+    //     let conditionElement = await driver.wait(until.elementLocated(By.css(`[data-testid="condition-${i}"]`)), 10000);
+    //     let conditionText = await conditionElement.getText();
+    //     assert.equal(conditionText, conditions[i]);
 
-        // if (conditionText === conditions[i]) {
-        //     await conditionElement.click();
-        //     bool = false;
-        // }
-        i++;
-    }
+    //     // if (conditionText === conditions[i]) {
+    //     //     await conditionElement.click();
+    //     //     bool = false;
+    //     // }
+    //     i++;
+    // }
 
-    await driver.wait(until.elementLocated(By.css(`[data-testid="condition-0"]`)), 10000).click();
+    // await driver.wait(until.elementLocated(By.css(`[data-testid="condition-0"]`)), 10000).click();
 
-    const element = await driver.wait(until.elementLocated(By.css(`[data-testid="conditional-logic-text"]`)));
-    // Move the cursor one character to the left using the LEFT arrow key
-    console.log('passed condition 1');
-    await new Promise((resolve) => setTimeout(resolve, 750));
-    await element.sendKeys(Key.ARROW_LEFT);
-    console.log('passed condition 2');
-    // Type '10' where the cursor is now placed
-    await new Promise((resolve) => setTimeout(resolve, 750));
-    await element.sendKeys('10');
+    // const element = await driver.wait(until.elementLocated(By.css(`[data-testid="conditional-logic-text"]`)));
+    // // Move the cursor one character to the left using the LEFT arrow key
+    // console.log('passed condition 1');
+    // await new Promise((resolve) => setTimeout(resolve, 750));
+    // await element.sendKeys(Key.ARROW_LEFT);
+    // console.log('passed condition 2');
+    // // Type '10' where the cursor is now placed
+    // await new Promise((resolve) => setTimeout(resolve, 750));
+    // await element.sendKeys('10');
     console.log('passed condition 3');
 });
 
