1a245f5966635c7b1ce0a3f5aba73cf6c437d8c3
Roopesh Yadava <roopesh.yadava@7edge.com>
2024-10-15 12:52:51 +0530

    RR-310 #time 20m feature: testcases for preview is added

---
+++ test/step-definations/admin/preview-questionnaire.js
@@ -0,0 +1,92 @@
+/* eslint-disable max-len */
+const assert = require('assert');
+const { When, Then } = require('@cucumber/cucumber');
+const webdriver = require("selenium-webdriver");
+const until = require('selenium-webdriver').until
+const By = require('selenium-webdriver').By
+const Keys = webdriver.Key;
+
+When('I enter the label name for textbox for preview', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('I should see the label name updated in the section {int} page {int} question {int}', function (int, int2, int3) {
+
+});
+
+
+When('I enter the help text for textbox for preview', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+
+Then('I should see the help text updated in the section {int} page {int} question {int}', function (int, int2, int3) {
+
+});
+
+
+When('I enter the placeholder content for textbox for preview', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+
+Then('I should see the placeholder content updated in the section {int} page {int} question {int}', function (int, int2, int3) {
+
+});
+
+When('I enter the label name for choice for preview', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('I enter the help text for choice for preview', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('I enter the placeholder content for choice for preview', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('I click on add new page for section {int}', function (int) {
+    // When('I click on add new page for section {float}', function (float) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('I should see the new page added', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('I enter the label name for date\\/time for preview', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+
+Then('I should see the label name for date\\/time updated in the section {int} page {int} question {int}', function (int, int2, int3) {
+
+});
+
+When('I enter the help text for date\\/time for preview', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('I should see the help text for date\\/time updated in the section {int} page {int} question {int}', function (int, int2, int3) {
+
+});
+
+When('I enter the placeholder content for date\\/time for preview', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('I should see the placeholder content for date\\/time updated in the section {int} page {int} question {int}', function (int, int2, int3) {
+
+});
