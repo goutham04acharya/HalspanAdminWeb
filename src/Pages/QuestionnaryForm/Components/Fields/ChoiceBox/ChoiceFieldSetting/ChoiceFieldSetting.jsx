08b99bdc885e53db318ba8f1281bf93465f9be8f
Dhanush J Alva <dalva@7edge.com>
2024-11-14 14:19:49 +0530

    RR-688 #time 2h bugfix:Fixed issues in the application mail to image upload issue removed default and placeholder from choice number in file extensions

--- src/Pages/QuestionnaryForm/Components/Fields/ChoiceBox/ChoiceFieldSetting/ChoiceFieldSetting.jsx
+++ src/Pages/QuestionnaryForm/Components/Fields/ChoiceBox/ChoiceFieldSetting/ChoiceFieldSetting.jsx
@@ -147,8 +147,9 @@ function ChoiceFieldSetting({
                     formParameters={formParameters}
                     handleBlur={handleBlur}
                     formStatus={formStatus}
+                    assetLocation
                 />
-                <div className='flex flex-col justify-start mt-7 w-full relative'>
+                {/* <div className='flex flex-col justify-start mt-7 w-full relative'>
                     <label htmlFor="Label" className='font-semibold text-base text-[#2B333B]'>Default Content</label>
                     <div className='relative w-full'>
                         <input
@@ -174,7 +175,7 @@ function ChoiceFieldSetting({
                             }:null}
                         />
                     </div>
-                </div>
+                </div> */}
                 <div className='mt-7'>
                     <p className='font-semibold text-base text-[#2B333B]'>Type</p>
                     <div className='mt-2.5'>
