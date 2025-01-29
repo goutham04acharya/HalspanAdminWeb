64f268eec9914a3deae35ee5ad166aaa5306c190
Roopesh Yadava <roopesh.yadava@7edge.com>
2025-01-29 19:03:28 +0530

    RR-797 #time 5m test: updated the path in cucumber file

--- test/integration-testing/Flow3/cucumber.js
+++ test/integration-testing/Flow3/cucumber.js
@@ -2,13 +2,14 @@ module.exports = {
     default: {
         import: ['test'],
         paths: [
-            'test/integration-testing/Flow2/1_login.feature',
+            'test/integration-testing/Flow3/1_login.feature',
             'test/integration-testing/Flow3/51_checkbox.feature',
             'test/integration-testing/Flow3/52_file_uploader.feature',
             'test/integration-testing/Flow3/53_time.feature',
             'test/integration-testing/Flow3/54_compliance_logic_basic_editor.feature',
             'test/integration-testing/Flow3/55_show_question_before_saving_in_editor.feature',
             'test/integration-testing/Flow3/57_show_choices_dropdown_editor.feature',
+            'test/integration-testing/Flow3/58_questionnaire_status.feature',
             'test/integration-testing/Flow2/2_logout.feature'
         ]
     }
