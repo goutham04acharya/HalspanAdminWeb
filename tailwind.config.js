9855763b1552ba175455cf2f415e92ef9250a713
pavitra <pavitra.kini@7edge.com>
2025-03-02 21:23:46 +0530

    RR-886 #time 15m bugfix:css issue is fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,10 +19,10 @@ export default {
                 'customh13': 'calc(100vh - 315px)',
                 'customh14': 'calc(100% - 20px)',
                 'customh15': 'calc(100vh - 135px)',
+                'customh15': 'calc(100vh - 135px)',
+                'customh15': 'calc(100vh - 155px)',
                 'customh16': 'calc(100vh - 524px)',
                 'customh17': 'calc(100vh - 200px)',
-
-
             },
             colors: {
                 'customTextColor': '#2B333B'
